710fa11bc2f7948582fa0d9d377ec9de536b0110
codeandcodeandcode <codepkcode49@gmail.com>
2022-06-23 06:18:03 +0300

    Custom tests and written solution of our work

--- index.js
+++ index.js
@@ -7,7 +7,12 @@ function isPalindrome(word) {
 */
 
 /*
-  Add written explanation of your solution here
+  
+
+    I need to make an isPalindrome function that returns either true or false. When the input string is the same forwards and backwards, I should return true. That means if the first letter is the same as the last letter, and the second letter is the same as the second to last letter, and so on (until the middle of the word), the function should return true.
+
+    For the word "racecar", the first and last letter is "r", the second and second to last is "a", the third and third to last is "c", and the middle is "e", so I should return true. For the word "robot", the first letter is "r" and the last letter is "t", so I should return false.
+
 */
 
 // You can run `node index.js` to view these console logs
@@ -17,6 +22,27 @@ if (require.main === module) {
   console.log("=>", isPalindrome("racecar"));
 
   console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("mom"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("hi"));
+
+  console.log("");
+
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
